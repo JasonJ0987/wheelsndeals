8366666a90f290b4d7113a3795cbe5f529f2e12b
Jason <chiyoung.jang@mail.mcgill.ca>
2023-04-25 14:04:33 -0400

    Completed API views of all related to sales

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -3,7 +3,10 @@ from django.db import models
 
 class AutomobileVO(models.Model):
     vin = models.CharField(max_length=17, unique=True)
+    sold = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.vin
 
 class Salesperson(models.Model):
     first_name = models.CharField(max_length=200)
