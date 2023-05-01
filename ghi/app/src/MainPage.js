be5f93812226e22475b0ca2a09c9bef967c7ef15
Jason <chiyoung.jang@mail.mcgill.ca>
2023-05-01 17:15:24 -0400

    minor edits

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -3,15 +3,15 @@ import './index.css';
 function MainPage() {
     return (
         <div className="px-4 py-5 text-center" id="background">
-        <h1 className="display-5 fw-bold">CarCar</h1>
+        <h1 id="h1Color" className="display-5 fw-bolder">Wheels N' Deals</h1>
         <div className="col-lg-6 mx-auto">
-            <p className="lead mb-4">
+            <p className="lead mb-4 textColor">
             The premiere solution for automobile dealership
             management!
             </p>
         </div>
         </div>
     );
-}
+};
 
 export default MainPage;
--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -5,7 +5,7 @@ function Nav() {
     return(
         <nav id="navcolor" className="navbar navbar-expand-lg navbar-dark">
         <div className="container-fluid">
-            <NavLink className="navbar-brand" to="/">CarCar</NavLink>
+            <NavLink className="navbar-brand" to="/">Wheels N' Deals</NavLink>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
