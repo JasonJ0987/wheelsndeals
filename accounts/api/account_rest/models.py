5b087b52b4bcf9b5928eb67d50c983db035d67de
Jason <chiyoung.jang@mail.mcgill.ca>
2023-05-02 12:50:40 -0400

    created accounts Models

--- accounts/api/account_rest/models.py
+++ accounts/api/account_rest/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class RoleVO(models.Model):
+    title=models.CharField(max_length=200)
+
+
+class Account(models.Model):
+    username = models.CharField(max_length=200, unique=True)
+    password = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    role = models.ForeignKey(
+        RoleVO,
+        related_name="account",
+        on_delete=models.CASCADE
+    )
+    
